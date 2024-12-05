eb2c6e33a669228f3e47f27c9fdc620a7fbdd314
Sean Outram <outramsean0@gmail.com>
2024-12-05 08:48:46 +0000

    Maybe working serial loop

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -16,7 +16,7 @@ namespace Launcher {
         graphics.rectangle(bg);
         // Do Text in corner
         //graphics.set_font(&DMSANS_SEMIBOLD_75_bitmap);
-        graphics.text("Revolution", pimoroni::Point(5, 1), 10, 2);
+        graphics.text("Revolution 2024", pimoroni::Point(5, 1), 10, 2);
 /*         FILE *file = fopen("test.js", "w");
         if (file != nullptr) {
             fprintf(file, "test1234");
--- src/lib/serial_api.cpp
+++ src/lib/serial_api.cpp
@@ -4,26 +4,27 @@
 #include <string>
 #include "pico/stdlib.h"
 #include "pfs.h"
+#include "dirent.h"
 
 enum INSTRUCTION_TYPE {
     NOT_SET = -1,
     CREATE = 48, // ASCII 0
-    DELETE = 49, // ASCII 1
-    GET = 50,    // ASCII 2
+    DELETE = 1
 };
+int INSTRUCTION = NOT_SET;
 
 std::string handle_longer_input(int buffer_length) {
     char data[buffer_length] = {0};
     int pos = 0;
     while (pos < sizeof(data) - 1) {
         int letter = getchar();
-        if (letter == '#' || letter == '\n' || letter == EOF) {
+        if (letter == ' ' || letter == '\n' || letter == EOF) {
             printf("Breaking character loop!\n");
              break;
         }
         data[pos] = static_cast<char>(letter);
         pos++;
-        sleep_ms(20);  // DO NOT FUCKING REMOVE THIS - SERIAL WILL BREAK
+        sleep_ms(10);
     }
     data[pos] = '\0';
     std::string data_str(data);
@@ -34,42 +35,21 @@ void handle_serial() {
     printf("cmd_ready");
     int ch = getchar();
     printf("%d\n", ch);
-    if (ch != EOF)  {
-        if (ch == CREATE) {
-            printf("ready\n");
-            std::string file_name_str = handle_longer_input(20);
-            printf("data_send\n");
-            std::string data_str = handle_longer_input(100);
-            printf("Data received: %s\n", data_str.c_str());
-            printf("File name received: %s\n", file_name_str.c_str());
-            FILE *file = fopen(file_name_str.c_str(), "w");
-            printf("Opened file\n");
-            if (file != nullptr) {
-                if (fprintf(file, data_str.c_str()) < 0) {
-                        printf("Error writing to file\n");
-                } else {
-                        printf("File written successfully\n");
-                }
-                fflush(stdout);
-                fclose(file);
-                printf("File created successfully\n");
-            } else {
-                printf("Failed to open file\n");
-            }
-            return;
-        } else if (ch == DELETE) {
-            printf("ready\n");
-            std::string file_name_str = handle_longer_input(20);
-            if (remove(file_name_str.c_str()) == 0) {
-                printf("File deleted successfully\n");
-            } else {
-                printf("Failed to delete file\n");
-            }
-            fflush(stdout);
+    if (ch != EOF && ch == CREATE) {
+        printf("ready\n");
+        std::string file_name_str = handle_longer_input(20);
+        printf("data_send\n");
+        std::string data_str = handle_longer_input(100);
+        printf("Data received: %s\n", data_str.c_str());
+        FILE *file = fopen(file_name_str.c_str(), "w");
+        if (file != nullptr) {
+            fprintf(file, data_str.c_str());
+            fclose(file);
+            printf("File created successfully\n");
         } else {
-            printf("Invalid command %c: %d\n", ch, ch);
+            printf("Failed to open file\n");
         }
     } else {
-        printf("EOF encountered\n");
+        printf("Invalid command or EOF encountered\n");
     }
 }
